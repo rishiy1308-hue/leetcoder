cfab7993841622a7b01b1afd1b4159fd9d5a74cd
rishiy1308-hue <rishiy1308@gmail.com>
2026-08-16 00:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (11.99%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans = "";
+        for(char ch:address){
+            if(ch != '.') ans.push_back(ch);
+            else{
+                ans.push_back('[');
+                ans.push_back(ch);
+                ans.push_back(']');
+            }
+        }
+        return ans;
+    }
+};
