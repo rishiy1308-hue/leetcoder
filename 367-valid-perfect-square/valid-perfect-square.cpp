d696916f6156a842a253a8bc2a491f2980b6ec11
rishiy1308-hue <rishiy1308@gmail.com>
2026-08-28 10:37:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (17.79%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long low = 1,high = num;
+        while(low<=high){
+            long long mid = low+(high-low)/2;
+            if(mid*mid>num) high = mid-1;
+            else if(mid*mid<num) low = mid+1;
+            else return true;
+        }
+        return false;
+    }
+};
