e538bac8d73421aefed0e6ccb3d8083e81a43965
rishiy1308-hue <rishiy1308@gmail.com>
2026-09-01 14:17:43 +0530

    Time: 4 ms (56.34%) | Memory: 18.9 MB (28.71%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -3,42 +3,34 @@ public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
 
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
 
-        // Build graph
-        for (auto &it : prerequisites) {
-            adj[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
+        for(auto it : prerequisites) {
+            int course = it[0];
+            int prerequisite = it[1];
+            adj[prerequisite].push_back(course);
         }
-
-        queue<int> q;
-
-        // Push all courses having indegree 0
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0)
-                q.push(i);
-        }
-
-        vector<int> ans;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            ans.push_back(node);
-
-            for (auto it : adj[node]) {
-                indegree[it]--;
-
-                if (indegree[it] == 0)
-                    q.push(it);
+        vector<int> inDegree(numCourses);
+        for(int i = 0;i<adj.size();i++){
+            for(auto it: adj[i]){
+                inDegree[it]++;
             }
         }
-
-        // Cycle exists
-        if (ans.size() != numCourses)
-            return {};
-
-        return ans;
+        queue<int> q;
+        for(int i = 0;i<inDegree.size();i++){
+            if(inDegree[i] == 0) q.push(i);
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                inDegree[it]--;
+                if(inDegree[it] == 0)q.push(it);
+            }
+        }
+        if(topo.size() != numCourses) return {};
+    
+        return topo;
     }
 };
