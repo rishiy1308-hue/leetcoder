764d1c485ef274d1a5663262bb35227f613cf1c1
rishiy1308-hue <rishiy1308@gmail.com>
2026-09-20 14:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int prod = 0;
+        for(int i = 0;i<s.size();i++){
+            prod+=(i+1)*('z'-s[i]+1);
+        }
+        return prod;
+    }
+};
